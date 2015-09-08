872df1a64c3c3a47a8467e83f3fb79bda5e03503
Daniel Brosseau <dab@catapulse.org>
2015-09-08 14:49:00 +0200

    rewrite code

--- lib/Catalyst/Plugin/Inject/MI.pm
+++ lib/Catalyst/Plugin/Inject/MI.pm
@@ -2,66 +2,30 @@ use utf8;
 package Catalyst::Plugin::Inject::MI;
 
 use Moose;
-use namespace::autoclean;
 use Dependency::Resolver;
-use Catalyst::Plugin::Inject::Module;
-
-has modules => (is      => 'rw',
-                isa     => 'ArrayRef',
-                default => sub { [] },
-               );
-
-has deps => ( is       => 'rw',
-              isa      => "ArrayRef|Catalyst::Plugin::Inject::Module",
-              default  => sub {[]},
-            );
 
 has resolver => (
               is       => 'rw',
               isa      => 'Dependency::Resolver',
-              default  => sub { Dependency::Resolver->new }
           );
-
-has name_to_ids => (is      => 'rw',
-                   isa     => 'HashRef',
-                   default => sub { {} },
-               );
 
 
 sub get_module {
-    my $self      = shift;
-    my $module    = shift;
-    my $operation = shift;
-    my $version   = shift;
+    my($self, $mod, $op , $ver) = @_;
 
-    if ( ! $operation || ! $version ) {
-        $operation = '>=';
-        $version   = 0;
-    }
-
-    my $ids_module = $self->name_to_ids->{$module};
-
-    # operation / version
-    # if operation : '>=' => uses the high version after or egal to 'version'
-    #                '>'  => uses the high version after to 'version'
-    #                '<'  => uses the high version under or egal to 'version'
-    #                '<=' => uses the high version under to 'version'
-
-    foreach my $id ( @$ids_module ) {
-        my $mod = ${$self->modules}[$id];
-        return $mod;
-        print "get_module: module=$module id=$id version:" . $mod->version . "\n";
-    }
+    my $modules = $self->resolver->get_modules($mod, $op, $ver);
+    return $modules->[-1];
 }
 
 sub resolv {
     my $self      = shift;
     my $module    = shift;
-    my $operation = shift;
-    my $version   = shift;
+    my $operation = shift || '>=';
+    my $version   = shift || 0;
 
-    my $moduleref = $self->get_module($module, $operation, $version );
-    $self->resolver->dep_resolv($moduleref);
+    my $module   = $self->get_module($module, $operation, $version );
+    my $resolved = $self->resolver->dep_resolv($module);
+    return $resolved;
 }
 
 sub load {
@@ -72,39 +36,20 @@ sub load {
     $conf_filename ||= 'config.yml';
     print "load_modules ...\n";
 
+    $self->resolver(Dependency::Resolver->new(debug => $conf->{debug}));
+
     # search modules in 'path' directories  and in @INC
-    for my $dir ( @{ $conf->{'path'} }, @INC ) {
-        $self->load_modules_path($dir, $conf_filename);
-    }
-    $self->convert_deps;
-}
-
-sub convert_deps{
-    my $self = shift;
-
-    foreach my $m ( @{$self->modules} ) {
-        my $i=0;
-        print "module:" . $m->name ."\n";
-        foreach my $name_op_version ( @{$m->deps} ) {
-
-            my ( $name, $operation, $version ) = split ( /\s+/, $name_op_version) ;
-            print "   dependence: $name\n";
-            print "   search module with good version ...\n";
-            # Converts the dependency name into a module object
-            # use the first module finded (without operation and version)
-            ${$m->deps}[$i] = $self->get_module($name);
-            $i++;
-        }
+    for my $dir ( @{ $conf->{path} }, @INC ) {
+        $self->load_modules_path($dir, $conf_filename, $conf->{debug});
     }
 }
-
-
 
 
 sub load_modules_path{
     my $self           = shift;
     my $dir            = shift;
     my $conf_filename  = shift;
+    my $debug          = shift;
 
     print "  - search modules on $dir ...\n";
 
@@ -127,22 +72,15 @@ sub load_modules_path{
                 or die "Error (conf: $mod_conf_filename) : $!\n";
             ($filename, $mod_config) = %{$cfg->[0]};
 
-            my $args = { name => $mod_name };
-            $args->{path} = $mod_path;
+            my $module = { name    => $mod_config->{name},
+                           version => $mod_config->{version},
+                           deps    => $mod_config->{deps},
+                           path    => $mod_path,
+                       };
 
-            $args->{version} = $mod_config->{version}
-                if ( defined $mod_config->{version} );
-
-            $args->{deps} = $mod_config->{dependencies}
-                if ( defined $mod_config->{dependencies} );
-
-            # adds the new module found in modules
-            push( @{ $self->modules },Catalyst::Plugin::Inject::Module->new( $args ));
-
-            push(@{$self->name_to_ids->{$mod_name}}, $id);
-            $id++;
+            $self->resolver->add($module);
         }
-        #else {  print "No config -> NEXT"; }
+        #else {  print "No config -> NEXT\n"; }
     }
 }
 
